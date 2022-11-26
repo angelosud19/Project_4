bd97e8f28672b4b0ae71a2c0e3604573b3b4e3b5
angelosud19 <angelo.sud19@gmail.com>
2022-11-26 00:42:37 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double x;
+        double y;
+
+        x = 10;
+        y = 30;
+
+        System.out.println(x + y);
+
+
+        int a;
+        int b;
+
+        a = -30;
+        b = 10;
+
+        System.out.println(a + b);
+
+        double c;
+        double d;
+
+        c = 0;
+        d = 0;
+
+        System.out.println(c + d);
+
+
+
+        // exercício simples de entrada, processamento e saída de dados (ESTRUTURA SEQUENCIAL)
+
+
+
+
+    }
+}
